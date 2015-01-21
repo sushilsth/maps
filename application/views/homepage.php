1ffda0ab5a758c2f52f4198bf036243c60ab83c9
sushilsth <sushilsth21@gmail.com>
2015-01-21 14:16:40 +0545

    left sidebar comp

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -3,24 +3,16 @@
         <title></title>
          <link rel="stylesheet" type="text/css" href="<?php echo base_url()."content/bootstrap/css/bootstrap.min.css"; ?>">
         <script src="<?php echo base_url()."content/bootstrap/js/bootstrap.js"?>" type="text/javascript"></script>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."content/styles.css"; ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."content/styles/styles.css"; ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."content/styles/animate.css"; ?>">
         <script src="<?php echo base_url().'content/jqueryEngine/jquery.js'; ?>" type="text/javascript"></script>
         <script>
             $(document).ready(function(){
 //                 alert("Asdf");
-                 $("#menu").click(function() { 
-                     
-            animate(".demo", 'bounce');  
-            return false;  
+                 $("#toggle").click(function() { 
+                   $("#menu").css("display","block");
+                   $("#menu").addClass('animated fadeInLeft');
         });  
-   
-      
-    function animate(element_ID, animation) {  
-        alert("Asfd");
-        $(element_ID).addClass(animation);  
-        var wait = window.setTimeout( function(){  
-            $(element_ID).removeClass(animation)}, 1300);  
-    }  
      });
        
         </script>
@@ -29,7 +21,7 @@
         <div class="navbar navbar-inverse navbar-static-top" style="padding: 0px;margin: 0px;">
                 <div class="container">
                 
-                    <a href="#" id="menu" class="toggle-nav btn nav navbar-nav navbar-inverse" title="Toggle Menu"><h4><span class="glyphicon glyphicon-align-justify"  style="margin-left:12px;"></span></h4></a>
+                    <a href="#" id="toggle" class="toggle-nav btn nav navbar-nav navbar-inverse" title="Toggle Menu"><h4><span class="glyphicon glyphicon-align-justify"  style="margin-left:12px;"></span></h4></a>
                     <div class="collapse navbar-collapse navHeaderCollapse">
                     <ul class="nav navbar-nav navbar-right">
                         <li></li>
@@ -39,8 +31,8 @@
                     </div>
                 </div>
             </div>
-        <div class="demo animated">
-        <div style="height: 100%;width: 20%;border: solid 1px;background: #222;" class="lightSpeedIn">
+        <div id="menu" style="display: none;">
+        <div style="height: 100%;width: 20%;border: solid 1px;background: #222;">
             <ul class="list-group">
                 <li class="list-group-item">hotels</li>
                 <li class="list-group-item">hotels</li>
